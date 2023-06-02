1b8046e54c213daf0a41696d8a4c4de2c56aeea3
vaniarevianaa@gmail.com <vaniarevianaa@gmail.com>
2023-06-02 20:45:22 +0700

    addloactions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,7 +9,7 @@
 // ***********************************************
 
 Cypress.Commands.add('login', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
     cy.get('.oxd-button').click()
