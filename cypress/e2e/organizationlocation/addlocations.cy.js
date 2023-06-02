29ff8bc341a9b765348d05f71c2326e527771822
vaniarevianaa@gmail.com <vaniarevianaa@gmail.com>
2023-06-02 21:06:05 +0700

    try

--- cypress/e2e/organizationlocation/addlocations.cy.js
+++ cypress/e2e/organizationlocation/addlocations.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://opensource-demo.orangehrmlive.com/ ')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
   })
 })
